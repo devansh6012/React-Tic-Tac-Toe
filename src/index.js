71a7a673316a51d2d853b493198f314cdd4dd1c3
devansh6012 <devansh.g6012@gmail.com>
2022-04-04 01:37:58 +0530

    Created Two Components Board and display. Added title and component in both of them

--- src/index.js
+++ src/index.js
@@ -2,10 +2,44 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import './index.css';
 
+class Board extends React.Component{
+    render(){
+        return(
+            <div className='board'>
+                <div className='title'>
+                    Tic Tac Toe
+                </div>
+                <div className='content'>
+
+                </div>
+            </div>
+        )
+    }
+}
+
+class Display extends React.Component{
+    render(){
+        let gameStatus = 'Next move for "X"';
+        return(
+            <div className='display'>
+                <div className='title'>
+                    {gameStatus}
+                </div>
+                <div className='content'>
+
+                </div>
+            </div>
+        )
+    }
+}
+
 class TTT extends React.Component{
     render(){
         return(
-            <div>Hello World</div>
+            <>
+                <Board/>
+                <Display/>
+            </>
         )
     }
 }
